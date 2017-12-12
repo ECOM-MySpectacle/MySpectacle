d98fc2f4d507f5e6757201c00386379fd7459491
geourjoa <anthony.geourjon@gmail.com>
2017-12-12 23:49:32 +0100

    Script pour le déploiement sur EC2

--- deployEC2v2.sh
+++ deployEC2v2.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-sudo docker stop tir
-sudo docker rm tir
+echo "[DEPLOY] Suppression d'instance de container en cours d'execution."
+
+sudo docker stop app
+sudo docker rm app
+
+echo "[DEPLOY] Pull des dernières modifications"
+
 git pull
+
+echo "[DEPLOY] Compilation et packaging de l'application "
 mvn clean compile package
+
+echo "[DEPLOY] Construction du conteneur"
+
 sudo docker build -t tirandule .
-sudo docker run --name=tir -p 8080:8080 tirandule > log &
+
+echo "[DEPLOY] Déploiement du conteneur 'app', logs disponible dans le fichier log  "
+
+sudo docker run --name=app -p 8080:8080 tirandule > log &
