2617c270a0fa9f4e6960ee05f599120f1c2b8fdc
geourjoa <anthony.geourjon@gmail.com>
2017-12-18 21:13:31 +0100

    Try to fix unit tests

---
+++ src/main/resources/MyTests.java
@@ -0,0 +1,16 @@
+
+public class MyTests {
+
+	import org.junit.Assert;
+	import org.junit.Test;
+
+	@Test
+	public void fakeTest() {
+		
+		// assert statements
+		assertEquals(true, true, "Test 1");
+		assertEquals(true, false, "Test 2");
+		
+	}
+
+}
