f37568e4a7fd4d4874112d65d14fd230c627a925
geourjoa <anthony.geourjon@gmail.com>
2017-12-18 19:27:53 +0100

    Mail and QRCode

--- src/main/java/org/applicationn/service/security/RegistrationMailSender.java
+++ src/main/java/org/applicationn/service/security/RegistrationMailSender.java
@@ -1,20 +1,42 @@
 package org.applicationn.service.security;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.imageio.ImageIO;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.servlet.ServletContext;
 
 import org.omnifaces.util.Faces;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.sun.jersey.multipart.BodyPart;
 
 /**
  * Utility class for sending mails for registration activation
@@ -23,7 +45,9 @@ import org.omnifaces.util.Faces;
  */
 public class RegistrationMailSender implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private static final String QR_CODE_FILE = "./QRCode.png";
+
+	private static final long serialVersionUID = 1L;
 
     private static final Logger logger = Logger.getLogger(RegistrationMailSender.class.getName());
 
@@ -31,8 +55,107 @@ public class RegistrationMailSender implements Serializable {
     private static final String REGISTRATION_MESSAGE = "registration message\nLink:";
     private static final String PW_RESET_SUBJECT = "password reset subject";
     private static final String PW_RESET_MESSAGE = "password reset message\nLink:";
+   
+    public static void sendQRCode(String to, String name, String idSpectacle, int nbPlaces, String nomSpectacle) {
+    	
+    	String subject = "Billeterie MySpetacle";
+    	String corps = "Bonjour, \nNous vous confirmons l'achat de " + nbPlaces + " du spectacle " + nomSpectacle + ".\n Veuillez trouver en pièce jointe votre e-billet sous la forme d'un QRCode. \n \n L'équipe MySpectacle";
+    	
+    	ServletContext context = Faces.getServletContext();
+    	String from = context.getInitParameter("mail.from");
+    	
+    	 try {
+
+             Message message = new MimeMessage(getSession(context));
+             message.setFrom(new InternetAddress(from));
+             message.setRecipients(Message.RecipientType.TO,
+                     InternetAddress.parse(to));
+             message.setSubject(subject);
+
+             // Create the message part
+             MimeBodyPart messageBodyPart = new MimeBodyPart();
+             // Now set the actual message
+             messageBodyPart.setText(corps);
+             // Create a multipar message
+             Multipart multipart = new MimeMultipart();
+             // Set text message part
+             multipart.addBodyPart(messageBodyPart);
+
+             // Part two is attachment
+             messageBodyPart = new MimeBodyPart();
+             
+             generateQRCode(name + "," + idSpectacle + "," + nbPlaces);
+             
+             logger.log(Level.INFO, "QRCode generated", to);
+             
+             String filename = QR_CODE_FILE;
+             DataSource source = new FileDataSource(filename);
+             messageBodyPart.setDataHandler(new DataHandler(source));
+             messageBodyPart.setFileName(filename);
+             multipart.addBodyPart(messageBodyPart);
+
+             // Send the complete message parts
+             message.setContent(multipart);      
+
+             logger.log(Level.INFO, "Sending QRCode to {0}", to);
+             
+             Transport.send(message);
+
+             logger.log(Level.INFO, "Mail sent succesfully!");
+
+         } catch (MessagingException e) {
+             throw new RuntimeException(e);
+         }
+    }
     
-    public static void sendRegistrationActivation(String to, String activationLink) {
+    
+    private static void generateQRCode(String myCodeText) {
+		
+    	String filePath = QR_CODE_FILE;
+		int size = 250;
+		String fileType = "png";
+		File myFile = new File(filePath);
+		try {
+			
+			Map<EncodeHintType, Object> hintMap = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+			hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+			
+			// Now with zxing version 3.2.1 you could change border size (white border size to just 1)
+			hintMap.put(EncodeHintType.MARGIN, 1); /* default = 4 */
+			hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+ 
+			QRCodeWriter qrCodeWriter = new QRCodeWriter();
+			BitMatrix byteMatrix = qrCodeWriter.encode(myCodeText, BarcodeFormat.QR_CODE, size,
+					size, hintMap);
+			int CrunchifyWidth = byteMatrix.getWidth();
+			BufferedImage image = new BufferedImage(CrunchifyWidth, CrunchifyWidth,
+					BufferedImage.TYPE_INT_RGB);
+			image.createGraphics();
+ 
+			Graphics2D graphics = (Graphics2D) image.getGraphics();
+			graphics.setColor(Color.WHITE);
+			graphics.fillRect(0, 0, CrunchifyWidth, CrunchifyWidth);
+			graphics.setColor(Color.BLACK);
+ 
+			for (int i = 0; i < CrunchifyWidth; i++) {
+				for (int j = 0; j < CrunchifyWidth; j++) {
+					if (byteMatrix.get(i, j)) {
+						graphics.fillRect(i, j, 1, 1);
+					}
+				}
+			}
+			ImageIO.write(image, fileType, myFile);
+		} catch (WriterException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	
+	}
+
+
+	public static void sendRegistrationActivation(String to, String activationLink) {
 
         ServletContext context = Faces.getServletContext();
         String from = context.getInitParameter("mail.from");
